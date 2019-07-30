69e781bc29f4745d44a8bab9d2611917559be6f9
Mohit Deshwal <mdeshwal80@gmail.com>
2019-07-30 15:06:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = jsonServer.create()
 const router = jsonServer.router('./database.json')
 const userdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'))
 
-//server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.urlencoded({extended: true}))
 //server.use(bodyParser.json())
 server.use(jsonServer.defaults());
 
